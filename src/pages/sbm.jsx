7aa4c7a6a507c5e917fe20ebdc08cbb434dec853
kanish2211 <jskanishka@gmail.com>
2022-12-04 08:41:44 +0530

    margin issues fixed in sbm

--- src/pages/sbm.jsx
+++ src/pages/sbm.jsx
@@ -9,7 +9,7 @@ const SBM=()=>{
             <p className="font-semibold mx-10 text-white text-4xl">At Uni, we're committed to <span className="onebythreegradientLong">delivering an unmatched credit experience</span> for millions of Indians.</p>
             
             <p className="text-lg mt-6 mx-10 text-gray-400">On this mission, we've partnered with some of the best in the business.</p>
-            <img className="mx-auto mt-10" src="https://www.uni.cards/images/SBM.svg"/>
+            <img className="mx-auto mt-" src="https://www.uni.cards/images/SBM.svg"/>
         </div>
         </div>
     )

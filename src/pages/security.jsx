bd57436b7bf4dc4982cc91b3c109b98ee4053159
kanish2211 <jskanishka2gmail.com>
2022-12-19 13:44:09 +0530

    margin bugs fixed

--- src/pages/security.jsx
+++ src/pages/security.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
-const Security =()=>{
-    return(
-        <div className="grey md:text-lg text-base text-gray-400 grid grid-cols-2 h-20  ">
-            <p className="text-center ml-auto mt-7 -mr-32 ">Uni maintains the highest level of security standards</p>
-            <img className="mr-auto mt-7 ml-36" src="https://www.uni.cards/images/pcidss_cert.svg"/>
-        </div>
-    )
-}
+const Security = () => {
+  return (
+    <div className="grey md:text-lg text-base text-gray-400 grid grid-cols-2 h-20  ">
+      <p className="text-center ml-auto mt-7 -mr-32 ">
+        Uni maintains the highest level of security standards
+      </p>
+      <img
+        className="mr-auto mt-7 ml-28"
+        src="https://www.uni.cards/images/pcidss_cert.svg"
+      />
+    </div>
+  );
+};
 
-export default Security
+export default Security;
